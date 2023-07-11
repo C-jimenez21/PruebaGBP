39f33b8222169f6b2ed20d2dcc82cf5fe4e7c365
C-jimenez21 <cristianjjc21@gmail.com>
2023-07-11 18:01:08 -0500

    feat: :sparkles: inicio punto 7

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ dotenv.config();
 const expressApp = express();
 
 expressApp.use(express.json());
+
 expressApp.use('/bodegas', appBodegas);
-expressApp.use('/productos', appProducto)
+expressApp.use('/productos', appProducto);
 
 
 let config = JSON.parse(process.env.SERVER_CONFIG);
+
 expressApp.listen(config, ()=>{
     console.log(`Server is running on port ${config.hostname}:${config.port}`);
 });
--- src/routers/appProductos.js
+++ src/routers/appProductos.js
@@ -20,4 +20,36 @@ appProducto.get('/:id?', (req, res) => {
     )
 })
 
+appProducto.post('/', (req, res)=>{
+    con.query(
+        `INSERT INTO productos SET ?`,
+        req.body,
+        (err, data, fils)=>{
+            console.log(err);
+            console.table(data);
+            res.status(data.affectedRows+200).send(data)
+        }
+    )
+   con.query(
+    `INSERT INTO inventarios SER ?`,
+    {
+        "id": 21,
+        "nombre": "DEFAULTalcrearProducto",
+        "descripcion": "Dulces y mas",
+        "estado": 1,
+        "created_by": 11,
+        "update_by": null,
+        "created_at": "2022-07-01T22:37:06.000",
+        "updated_at": null,
+        "deleted_at": null
+      }
+   ),
+   (err, data, fils) =>{
+        console.log(err)
+        console.table(data)
+        res.status(200).send(data);
+   }
+})
+
+
 export default appProducto;
